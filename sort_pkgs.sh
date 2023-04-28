f803f90430cc2afa86d9c8c43970f3a063c54987
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2023-04-28 03:41:44 +0300

    Simplified the logic to work directly on either an input file, or '/dev/stdin'.

--- sort_pkgs.sh
+++ sort_pkgs.sh
@@ -3,13 +3,12 @@
 # Description:	Sort the package list in 'pkg.tsv' by category, short, name.
 #
 
-FNAME="$1"
+# If user passed argument, treat it as input file name, else use /dev/stdin
+{
+	# Read, and output the header as is.
+	read header
+	echo "$header"
+	# Sort the packages
+	sort -t"$(printf '\t')" -k3,3 -k4,4 -k1,1 < /dev/stdin
+} < ${1:-/dev/stdin}
 
-hfile=$(mktemp)
-dfile=$(mktemp)
-
-head -n1 $FNAME > $hfile
-tail -n+2 $FNAME | sort -t"$(printf '\t')" -k3,3 -k4,4 -k1,1 > $dfile
-
-cat $hfile $dfile
-rm -rf $hfile $dfile
