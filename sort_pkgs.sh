526e98601bd74caac5991e16bf3896f6f2baee23
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-04-16 21:18:00 +0200

    Changed the packages sorting order.

--- sort_pkgs.sh
+++ sort_pkgs.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 #
-# Description:	Sort the package list in 'pkg.tsv' by category, short, name.
+# Description:	Sort the package list in 'pkg.tsv' by:
+#               1. essential    (reverse)
+#               2. server       (reverse)
+#               2. ui
+#               3. category
+#               4. short
+#               5. name
 #
 
 # If user passed argument, treat it as input file name, else use /dev/stdin
@@ -9,6 +15,6 @@
 	read header
 	echo "$header"
 	# Sort the packages
-	sort -t"$(printf '\t')" -k3,3 -k4,4 -k1,1 < /dev/stdin
+	sort -t"$(printf '\t')" -k10r,10 -k13r,13 -k9,9 -k3,3 -k4,4 -k1,1 # < /dev/stdin
 } < "${1:-/dev/stdin}"
 
